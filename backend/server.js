7cc457a647247f762cb26b9a55f0ab18d508c279
tgalen <tgalen84@gmail.com>
2022-05-14 11:04:13 -0400

    add mock api

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,11 @@
-console.log("hello World");
+const express = require("express");
+const dotenv = require("dotenv").config();
+const port = process.env.PORT || 5000;
+
+const app = express();
+
+app.get("/api/vinylcollection", (req, res) => {
+  res.status(200).json({ message: "get albums" });
+});
+
+app.listen(port, () => console.log(`Server started on ${port}`));
