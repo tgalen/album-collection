e72f69c1e0f79a0ad0fe2b5c907e60e5fdbc9691
tgalen <tgalen84@gmail.com>
2022-05-18 21:29:38 -0400

    add album and artist name to CollectedRecord display

--- frontend/src/components/CollectedRecord.js
+++ frontend/src/components/CollectedRecord.js
@@ -1,7 +1,11 @@
+import SearchedAlbum from "./SearchedAlbum";
+
 const CollectedRecord = ({ recordDetails }) => {
   return (
     <div>
       <img alt={recordDetails.name} src={recordDetails.images[1].url} />
+      <h5>{recordDetails.name}</h5>
+      <h5>{recordDetails.artists[0].name}</h5>
     </div>
   );
 };
