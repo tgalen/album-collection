7c5d9947a40df1d75ec0ff38747c468f51297b90
tgalen <tgalen84@gmail.com>
2022-05-23 19:04:34 -0400

    add variable to list all artists with no duplicate artists and add helper function to get images of an artists cover art

--- frontend/src/components/Collection.js
+++ frontend/src/components/Collection.js
@@ -68,9 +68,54 @@ const Collection = ({ collection }) => {
     e.target.style.background = "lightblue";
   };
 
+  const filterDuplicateArtists = collection.filter(
+    (record, index) => collection.indexOf(record.artists.name) === index
+  );
+
+  console.log(filterDuplicateArtists);
+
+  const collectedArtists = collection.map((record) => {
+    return record.artists[0].name;
+  });
+
+  console.log(collectedArtists);
+
+  const filterCollectedArtistsDuplicates = collectedArtists.filter(
+    (artist, index) => collectedArtists.indexOf(artist) === index
+  );
+
+  console.log(filterCollectedArtistsDuplicates);
+
+  const getArtistRecordCoverArt = (artist) => {
+    // returns an array of objects with each object being the small image album art
+    const artistRecords = collection.filter((album) => {
+      return album.artists[0].name === artist;
+    });
+    const artistRecordCovers = artistRecords.map((album) => {
+      return album.images[2];
+    });
+    return artistRecordCovers;
+  };
+
+  console.log(getArtistRecordCoverArt("The Beatles"));
+
   return (
     <div>
       <h1>Collection</h1>
+      <div style={alphabetContainerStyle}>
+        {alphabet.map((letter) => {
+          return (
+            <div
+              style={letterContainerStyle}
+              onMouseEnter={handleMouseEnterLetter}
+              onMouseLeave={handleMouseLeaveLetter}
+              key={letter}
+            >
+              {letter}
+            </div>
+          );
+        })}
+      </div>
       <div style={collectionContainerStyle}>
         {collection ? (
           collection.map((album) => {
@@ -86,19 +131,27 @@ const Collection = ({ collection }) => {
           <h1>Such empty. Add some records by searching</h1>
         )}
       </div>
-      <div style={alphabetContainerStyle}>
-        {alphabet.map((letter) => {
-          return (
-            <div
-              style={letterContainerStyle}
-              onMouseEnter={handleMouseEnterLetter}
-              onMouseLeave={handleMouseLeaveLetter}
-              key={letter}
-            >
-              {letter}
-            </div>
-          );
-        })}
+      <div>
+        <h2>Artists</h2>
+        <div>
+          {filterCollectedArtistsDuplicates.map((artist) => {
+            return (
+              <div
+                style={{
+                  width: "10%",
+                  height: "5%",
+                  border: "1px solid black",
+                }}
+              >
+                <h4>{artist}</h4>
+                <img
+                  src="https://img.icons8.com/ios-filled/50/000000/music-record.png"
+                  style={{ display: "inline-block" }}
+                />
+              </div>
+            );
+          })}
+        </div>
       </div>
     </div>
   );
