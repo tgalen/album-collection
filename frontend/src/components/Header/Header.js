49752b7689d5d62b93a477bd118c1552774138bd
tgalen <tgalen84@gmail.com>
2022-09-08 19:08:07 -0400

    edit logo

--- frontend/src/components/Header/Header.js
+++ frontend/src/components/Header/Header.js
@@ -1,6 +1,6 @@
 import { Link } from "react-router-dom";
 import "./Header.css";
-import logo from "../../assets/recordkeeperlogo.png";
+import logo from "../../assets/RKLogocd4e00whiterecordbg.png";
 // const headerStyle = {
 //   padding: "2%",
 //   position: "relative",
