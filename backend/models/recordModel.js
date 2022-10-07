952a48137e7a9934b894de7ba27fe2c6ef7e1bb2
tgalen <tgalen84@gmail.com>
2022-10-07 13:17:04 -0400

    update schema

--- backend/models/recordModel.js
+++ backend/models/recordModel.js
@@ -2,26 +2,13 @@ const mongoose = require("mongoose");
 
 const recordSchema = mongoose.Schema(
   {
-    album_type: {
-      type: String,
-      required: [true, "Please add an album_type field"],
-    },
-    artists: [],
-    spotify_url: String, // external_urls.spotify
-    href: String,
-    spotify_id: {
-      type: String,
-      required: [true, "Please add an spotify_id field"],
-    },
+    artist: String,
     images: [],
     name: {
       type: String,
       required: [true, "Please add a name field"],
     },
-    release_date: String,
-    total_tracks: Number,
-    type: String,
-    uri: String,
+    url: String,
     collectedUsers: [
       { type: mongoose.Schema.Types.ObjectId, ref: "user", unique: true },
     ],
