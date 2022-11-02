04b3f673ca25b7a6c87740f48a9334d6e1ab75cb
tgalen <tgalen84@gmail.com>
2022-11-02 17:50:03 -0400

    add links to navbar

--- frontend/src/components/Navbar/Navbar.js
+++ frontend/src/components/Navbar/Navbar.js
@@ -1,7 +1,33 @@
+import { Link } from "react-router-dom";
+import { useSelector } from "react-redux";
 import "./Navbar.css";
 
 const Navbar = () => {
-  return <div className="navbar">Navbar</div>;
+  const { user } = useSelector((state) => state.auth);
+  return (
+    <div className="navbar">
+      <ul className="nav-menu">
+        {!user && (
+          <li>
+            <Link to="/register">Create Account</Link>
+          </li>
+        )}
+        {!user && (
+          <li>
+            <Link to="/login">Login</Link>
+          </li>
+        )}
+
+        <li>
+          <Link to={user ? "/dashboard" : "/"}>Home</Link>
+        </li>
+
+        <li>
+          <Link to="/searchusers">Search Users</Link>
+        </li>
+      </ul>
+    </div>
+  );
 };
 
 export default Navbar;
