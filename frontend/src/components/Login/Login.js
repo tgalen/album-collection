50bbe97f013ef29be0fd1294b957640e6c4fe44a
tgalen <tgalen84@gmail.com>
2022-11-09 10:19:29 -0500

    style login form

--- frontend/src/components/Login/Login.js
+++ frontend/src/components/Login/Login.js
@@ -1,4 +1,5 @@
 import { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
 import { FaSignInAlt } from "react-icons/fa";
 import { useSelector, useDispatch } from "react-redux";
 import { useNavigate } from "react-router-dom";
@@ -57,45 +58,50 @@ const Login = () => {
   }
 
   return (
-    <div className="login-form-container">
-      <section className="heading">
-        <h1>
-          <FaSignInAlt /> Login
-        </h1>
-        <p>Login to your account</p>
-      </section>
-      <section className="form">
-        <form onSubmit={onSubmit}>
-          <div className="form-group">
-            <input
-              type="email"
-              className="form-control"
-              id="email"
-              name="email"
-              value={email}
-              placeholder="Enter Email"
-              onChange={onChange}
-            ></input>
-          </div>
-          <div className="form-group">
-            <input
-              type="password"
-              className="form-control"
-              id="password"
-              name="password"
-              value={password}
-              placeholder="Enter Password"
-              onChange={onChange}
-            ></input>
-          </div>
+    <div className="login-content-container">
+      <div className="login-form-container">
+        <section className="heading">
+          <h1>
+            <FaSignInAlt /> Login
+          </h1>
+          <p>Login to your account</p>
+        </section>
+        <section className="form">
+          <form onSubmit={onSubmit}>
+            <div className="form-group">
+              <input
+                type="email"
+                className="form-control"
+                id="email"
+                name="email"
+                value={email}
+                placeholder="Enter Email"
+                onChange={onChange}
+              ></input>
+            </div>
+            <div className="form-group">
+              <input
+                type="password"
+                className="form-control"
+                id="password"
+                name="password"
+                value={password}
+                placeholder="Enter Password"
+                onChange={onChange}
+              ></input>
+            </div>
 
-          <div className="form-group">
-            <button type="submit" className="btn btn-block">
-              Submit
-            </button>
-          </div>
-        </form>
-      </section>
+            <div className="form-group">
+              <p>
+                <Link to="/register">no account? please register</Link>
+              </p>
+              <button type="submit" className="btn btn-block">
+                Submit
+              </button>
+            </div>
+          </form>
+        </section>
+      </div>
     </div>
   );
 };
