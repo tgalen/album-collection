93ab7174929105e71e98886038bad0dd1858b931
tgalen <tgalen84@gmail.com>
2022-11-11 17:14:19 -0500

    styling

--- frontend/src/components/RecordDisplay/RecordDisplay.js
+++ frontend/src/components/RecordDisplay/RecordDisplay.js
@@ -1,13 +1,14 @@
 import "./RecordDisplay.css";
 
 const RecordDisplay = ({ record }) => {
+  let image = record.images.medium;
   return (
-    <div
-      className="record-display-container"
-      style={{
-        backgroundImage: `url("${record.images.large}`,
-      }}
-    ></div>
+    <div className="record-display-container">
+      <img src={record.images.large} />
+      <div className="record-title-container">
+        <h4>{record.name}</h4>
+      </div>
+    </div>
   );
 };
 
