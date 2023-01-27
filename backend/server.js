cb73b2b9d559b17bdea944cd1343a2d72e847c3d
tgalen <tgalen84@gmail.com>
2023-01-27 11:18:16 -0500

    prepare deploy

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+const path = require("path");
 const express = require("express");
 const dotenv = require("dotenv").config();
 const colors = require("colors");
@@ -22,6 +23,19 @@ app.use(express.urlencoded({ extended: false }));
 app.use("/api/records", require("./routes/recordRoutes"));
 app.use("/api/users", require("./routes/userRoutes"));
 app.use("/search", require("./routes/searchRoutes"));
+
+// Serve frontend
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/build")));
+
+  app.get("*", (req, res) =>
+    res.sendFile(
+      path.resolve(__dirname, "../", "frontend", "build", "index.html")
+    )
+  );
+} else {
+  app.get("/", (req, res) => res.send("Please set to production"));
+}
 
 app.use(errorHandler);
 
