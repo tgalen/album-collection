fc5dd0cfbc24e8a8f1bd9b1f4e36a7bd2ed24a97
tgalen <tgalen84@gmail.com>
2023-01-27 15:00:26 -0500

    adjust API

--- frontend/src/features/auth/authService.js
+++ frontend/src/features/auth/authService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:5000/api/users/";
+const API_URL = "https://recordkeepr.herokuapp.com/api/users/";
 
 // register user
 const register = async (userData) => {
